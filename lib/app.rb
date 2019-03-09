9646449eddf9cdf8cd8ef28d3d40e0b4b99d3392
Kevin McKelvin <kmckelvin@gmail.com>
2019-03-09 16:56:04 +0000

    Add frozen string literal comment

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,6 @@
-# app.rb
-require 'sinatra'
-require 'sinatra/activerecord'
+# frozen_string_literal: true
+
+require "sinatra"
+require "sinatra/activerecord"
 
 set :database, ENV["DATABASE_URL"] || "postgres://postgres:secret@127.0.0.1/catcher"
